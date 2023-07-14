d68232d39649f8b0564e1d278bd7cfc8aac0092e
Pawan Singh Sisodiya <pawan.s.bikharniya@gmail.com>
2023-07-14 10:08:50 +0530

    Navbar Updated

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
     <>
     <nav>
     <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
-            <Link className="text-white font-bold" href={'/'}>GTCoding</Link>
-            <Link className="bg-white p-2" href={'/addTopic'}>Add Topic</Link>
+            <Link className="text-white font-bold" href={'/'}>EMS</Link>
+            <Link className="bg-white p-2" href={'/addTopic'}>Add Employee</Link>
         </nav>
     </nav>
     </>
